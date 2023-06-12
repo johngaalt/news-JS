171fe8574917c4a3d790b4c4b20f858ef43978d0
johngaalt <rajon777666@gmail.com>
2023-06-12 12:40:14 +0300

    fix: use proxy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: 'da412dadd3024699bbd9f126281e9090', // получите свой ключ https://newsapi.org/
         });
     }
